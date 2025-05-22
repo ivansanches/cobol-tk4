58dcd4a135f62666ca8d84471f96e21c863c3cc3
ivansanches <ivansanches1972@gmail.com>
2025-05-22 15:36:26 -0300

    Create prog1.cob

---
+++ cobol/prog1.cob
@@ -0,0 +1,7 @@
+       IDENTIFICATION     DIVISION.          
+       PROGRAM-ID. PROG1.                    
+       ENVIRONMENT        DIVISION.          
+       DATA               DIVISION.          
+       PROCEDURE          DIVISION.          
+           DISPLAY '--PRIMEIRO PROGRAMA--'.  
+           STOP RUN.                         
